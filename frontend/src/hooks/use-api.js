74a157779c60b2f4fa9ae58b3e48f8c298d2ee5b
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2022-01-23 22:26:56 +0200

    infinite loading fix 2

--- frontend/src/hooks/use-api.js
+++ frontend/src/hooks/use-api.js
@@ -1,44 +1,46 @@
-import axios from 'axios'
-import { useCallback } from 'react'
+import axios from "axios";
+import { useCallback, useMemo } from "react";
 
 const instance = axios.create({
-  baseURL: 'https://api.vdsm-20h.online'
-  }
-)
+  baseURL: "https://api.vdsm-20h.online",
+});
 
 const useApi = () => {
   const createMeeting = useCallback(async () => {
-    const response = await instance.post('/jitsi/create')
-    if (response.status === 200){
+    const response = await instance.post("/jitsi/create");
+    if (response.status === 200) {
       return response.data;
     }
     //TODO: error handling
-  }, [])
+  }, []);
 
   const createInvite = useCallback(async (jwt) => {
-    const response = await instance.post('/jitsi/invite', null, {
+    const response = await instance.post("/jitsi/invite", null, {
       headers: {
-        Authorization: `Bearer ${jwt}`
-      }
-    })
-    if (response.status === 200){
-      return response.data
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    if (response.status === 200) {
+      return response.data;
     }
     //TODO: error handling
-  }, [])
+  }, []);
 
-  const getInfoByAlias = useCallback(async (alias) =>{
-    const response = await instance.get('/jitsi/' + alias)
-    if (response.status === 200){
-      return response.data
+  const getInfoByAlias = useCallback(async (alias) => {
+    const response = await instance.get("/jitsi/" + alias);
+    if (response.status === 200) {
+      return response.data;
     }
-  }, [])
+  }, []);
 
-  return {
-    createMeeting,
-    createInvite,
-    getInfoByAlias
-  }
-}
+  return useMemo(
+    () => ({
+      createMeeting,
+      createInvite,
+      getInfoByAlias,
+    }),
+    [createMeeting, createInvite, getInfoByAlias]
+  );
+};
 
-export default useApi
+export default useApi;
