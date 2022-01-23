838dadddc68460408af7c2ceec5c95b7c465d496
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2022-01-23 23:14:33 +0200

    infinite loading fix 2

--- frontend/src/hooks/use-api.js
+++ frontend/src/hooks/use-api.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
-import { useCallback } from 'react'
-import { toast } from 'react-toastify'
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const instance = axios.create({
   baseURL: "https://api.vdsm-20h.online",
@@ -11,48 +11,54 @@ const useApi = () => {
   const navigate = useNavigate();
   const createMeeting = useCallback(async () => {
     try {
-      const response = await instance.post('/jitsi/create')
+      const response = await instance.post("/jitsi/create");
       if (response.status === 200) {
         return response.data;
       }
-    } catch(err) {
-      toast.error('Can\'t create the meeting! Try again.');
+    } catch (err) {
+      toast.error("Can't create the meeting! Try again.");
     }
   }, []);
 
   const createInvite = useCallback(async (jwt) => {
     try {
-      const response = await instance.post('/jitsi/invite', null, {
+      const response = await instance.post("/jitsi/invite", null, {
         headers: {
-          Authorization: `Bearer ${jwt}`
+          Authorization: `Bearer ${jwt}`,
         },
       });
       if (response.status === 200) {
-        return response.data
+        return response.data;
       }
-      return {}
+      return {};
     } catch (err) {
-      toast.error('Can\'t create the invitation! Try again.');
+      toast.error("Can't create the invitation! Try again.");
     }
   }, []);
 
-  const getInfoByAlias = useCallback(async (alias) =>{
-    try {
-      const response = await instance.get('/jitsi/' + alias)
-      if (response.status === 200) {
-        return response.data
+  const getInfoByAlias = useCallback(
+    async (alias) => {
+      try {
+        const response = await instance.get("/jitsi/" + alias);
+        if (response.status === 200) {
+          return response.data;
+        }
+      } catch {
+        toast.error("Can't join the meeting! Check the link or meeting id.");
+        navigate("/", { replace: true });
       }
-    } catch {
-      toast.error('Can\'t join the meeting! Check the link or meeting id.');
-      navigate('/', { replace: true });
-    }
-  }, [navigate])
+    },
+    [navigate]
+  );
 
-  return {
-    createMeeting,
-    createInvite,
-    getInfoByAlias
-  }
-}
+  return useMemo(
+    () => ({
+      createMeeting,
+      createInvite,
+      getInfoByAlias,
+    }),
+    [createMeeting, createInvite, getInfoByAlias]
+  );
+};
 
 export default useApi;
